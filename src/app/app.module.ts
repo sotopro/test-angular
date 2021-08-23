28ca42bbafd038d258f46ed56ba45d4eee585225
Daniel Soto <dasg.1993@gmail.com>
2021-08-23 13:34:57 -0500

    [feature] - generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ListComponent } from './components/list/list.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    ItemListComponent,
+    ModalComponent,
+    SnackbarComponent,
+    PaginatorComponent
   ],
   imports: [
     BrowserModule
